ab3b5fbe21b1ee907b21bfeb6f8f1d3520952908
lukee <luke.ekkizogloy@gmail.com>
2016-02-16 19:56:06 -0800

    Adding JSON test code

--- src/js/vcookbookgen.js
+++ src/js/vcookbookgen.js
@@ -1,17 +1,52 @@
-;VCBG = {
-  parse_cookbook: function(data) {
-    //TODO
-    console.log("parse_cookbook");
-    return 0;
-  },
+// ------------------------------------------------------------------
+// Vipassana Cookbook Generator
+//    Luke Ekkizogloy
+//
+// This is the framework to enable Vipassana Meditation Centers
+// to select recipes & generate special instructions for custom 
+// cookbooks.
+//
+// Currently the recipes will be in JSON files for the first step
+// and then will be converted to basic text files as a second step
+// (if needed)
+//
+// Upon loading, the program will scan for existing recipe files and
+// will populate a webpage with data based on the recipes available.
+//
+// ------------------------------------------------------------------
 
-  test_f: function(x,y) {
-    console.log("test_f");
-    return (x*y);
+
+;VCBG = {
+
+  // Read a single recipe file in JSON format into a datastructure
+  read_recipe_json: function(json_file) {
+    console.log("read_recipe_json("+json_file+")");
+
+    var rec = $.getJSON(json_file, function(data) {
+      console.log("success");
+      console.log("Data:"+data.menu.id);
+    })
+      .done(function() {
+        console.log("second success ... done");
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      })
+      .complete(function() {
+        console.log("second complete");
+      });
+
+      return 0;
   }
 }
 
 $(document).ready(function() {
+
+  VCBG.read_recipe_json("nomz.json");
+
   $.get('test.txt', function(data) {
     $("h1").html("oh hi");
   }, "text");
